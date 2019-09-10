5753a81d52b298e133487f1d2b84837855888775
ruralkan <ruralkan@gmail.com>
2019-09-09 23:50:59 -0500

    Pass Test of container protocol

--- implementingCollections/sorted_set.py
+++ implementingCollections/sorted_set.py
@@ -1,3 +1,11 @@
 class SortedSet:
     def __init__(self, items = None):
         self._items = sorted(items) if items is not None else []
+    
+    """
+    dunder contain is the most fundamental of the collection protocols and simply allows us to determine whether a particular item is present in the collection
+    dunder contains accept a single argument, which is the item to test for and returns a Boolean
+    Our dunder contains implementation will just use the membership test on the enclosed list object
+    """
+    def __contains__(self, item):
+        return item in self._items
